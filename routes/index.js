7ed0da19d6bda234addc0973ede4e876976cfa4e
Ipung Purwono 201501138 <ipungofficial@gmail.com>
2018-07-13 18:36:01 +0700

    menampilkan data event di homepage

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Event = require('../models/Event');
 
 /* GET home page. */
 //ambil tampilan render dari folder view berformat ejs
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Purwokerto Js' });
+  //res.render('pages/index', { title: 'Purwokerto Js' });
+  //Tampilkan data dari mongodb
+  Event.find({}, function (err, event) {
+    console.log(event);
+    res.render('pages/index', {events: event ,title: 'Purwokerto Js'});
+  }).select('judul tanggal penyelenggara tipe mulai selesai foto keterangan');
 });
 
 module.exports = router;
