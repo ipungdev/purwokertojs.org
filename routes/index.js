a6f1617aa4c5a1069d448d2b79b4f4fc0562c376
Ipung Purwono 201501138 <ipungofficial@gmail.com>
2018-07-22 22:51:15 +0700

    Fitur filter atau pencarian data event berhasil

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var Event = require('../models/Event');
 /* GET home page. */
 //ambil tampilan render dari folder view berformat ejs
 //tampilkan data event dengan limit 6 data saja.
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   //res.render('pages/index', { title: 'Purwokerto Js' });
   //Tampilkan data dari mongodb
   Event.find({}, function (err, event) {
     console.log(event);
-    res.render('pages/index', {events: event ,title: 'Purwokerto Js'});
+    res.render('pages/index', { events: event, title: 'Purwokerto Js' });
   }).select('judul tanggal penyelenggara tipe mulai selesai foto keterangan').limit(6);
 });
 
@@ -18,18 +18,42 @@ router.get('/', function(req, res, next) {
 //Menampilkan Detail Event
 router.get('/detail/:id', function (req, res) {
   Event.findOne({ _id: req.params.id }, function (err, event) {
-      if (event) {
-          console.log(event);
-          res.render('pages/detailevent', { title: "Judul Event", event: event });
+    if (event) {
+      console.log(event);
+      res.render('pages/detailevent', { title: "Judul Event", event: event });
 
 
-      }
-      else {
-          req.flash('msg_error', 'Maaf, event tidak ditemukan!');
-          res.redirect('http://localhost:3000/');
-      }
+    }
+    else {
+      req.flash('msg_error', 'Maaf, event tidak ditemukan!');
+      res.redirect('http://localhost:3000/');
+    }
   });
 });
+
+
+
+
+//Pencarian Data Event
+router.post('/cari/', function (req, res) {
+  var item = req.body.judul;
+  var tanggal = req.body.tanggal;
+  console.log(item);
+  console.log(tanggal);
+
+  Event.find({ judul: new RegExp(item, 'i'), tanggal: tanggal }, function (err, event) {
+    if (event) {
+      console.log(event);
+      //res.render('pages/detailevent', { title: "Judul Event", event: event });
+    }
+    else {
+      console.log('Salah');
+      // req.flash('msg_error', 'Maaf, event tidak ditemukan!');
+      //res.redirect('http://localhost:3000/');
+    }
+  });
+});
+
 
 
 module.exports = router;
--- routes/meetup.js
+++ routes/meetup.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var Meetup = require('../models/Meetup');
 
 /* GET meetup page. */
 //ambil tampilan render dari folder view berformat ejs
+
+
 router.get('/', function(req, res, next) {
-  res.render('pages/meetup', { title: 'Meetup Purwokerto Js' });
+  //res.render('pages/index', { title: 'Purwokerto Js' });
+  //Tampilkan data dari mongodb
+  Meetup.find({}, function (err, meetup) {
+    console.log(meetup);
+    res.render('pages/meetup', {meetup: meetup ,title: 'Meetup Purwokerto Js'});
+  }).select('judul sub foto keterangan').limit(9);
+});
+
+//Menampilkan Detail Meetup
+router.get('/detail/:id', function (req, res) {
+  Meetup.findOne({ _id: req.params.id }, function (err, meetup) {
+      if (meetup) {
+          console.log(meetup);
+          res.render('pages/detailmeetup', { title: "Judul Event", meetup: meetup });
+
+
+      }
+      else {
+          req.flash('msg_error', 'Maaf, event tidak ditemukan!');
+          res.redirect('http://localhost:3000/');
+      }
+  });
 });
 
 module.exports = router;
